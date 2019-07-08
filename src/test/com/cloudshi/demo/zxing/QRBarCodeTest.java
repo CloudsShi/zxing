9994e52187713af9516292411eb3298660ab7fa1
石霄 <791235286@qq.com>
2019-07-08 18:47:43 +0800

    小型测试

--- src/test/com/cloudshi/demo/zxing/QRBarCodeTest.java
+++ src/test/com/cloudshi/demo/zxing/QRBarCodeTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -16,13 +18,14 @@ public class QRBarCodeTest {
         QRBarCodeUtil.createCodeToFile("https://www.baidu.com/",
                 new File("./"), "baidu.png");
 
-        QRBarCodeUtil.createCodeToOutputStream("https://www.baidu.com/",
-                System.out);
+//        QRBarCodeUtil.createCodeToOutputStream("https://www.baidu.com/", System.out);
     }
 
     @Test
-    public void parseTest() {
-        System.out.println(QRBarCodeUtil.parseQRCodeByFile(new File("./baidu.png")));
+    public void parseTest() throws MalformedURLException {
+//        System.out.println(QRBarCodeUtil.parseQRCodeByFile(new File("./baidu.png")));
+
+        System.out.println(QRBarCodeUtil.parseQRCodeByUrl(new URL("https://res.wx.qq.com/mpres/htmledition/images/mp_qrcode3a7b38.gif")));
     }
 
 
